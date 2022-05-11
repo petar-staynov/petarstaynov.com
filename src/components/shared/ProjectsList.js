d8cce404898e5dfa8302cf1b1388664caba7e809
Petar Staynov <petar.staynov@gmail.com>
2022-05-11 17:49:12 +0300

    Added print pagebreaks

--- src/components/shared/ProjectsList.js
+++ src/components/shared/ProjectsList.js
@@ -3,7 +3,7 @@ import ProjectsListItem from "./ProjectsListItem";
 
 const ProjectsList = ({ title, projects }) => {
   return (
-    <section>
+    <section className="pagebreak">
       <h3 className="text-gray-700">{title}:</h3>
       {projects.map((project, index) => (
         <ProjectsListItem key={index} project={project} title={title} />
